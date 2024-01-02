6c20aa4e3539767d63d0e7086470eb265cb282fc
danniwide1234 <danniwide.1981@gmail.com>
2024-01-02 15:44:24 -0800

    1-last_digit.py

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-digitalize = abs(number) % 10
-if number < 0:
-    digitalize = -digitalize
-print(f"Last digitalize of {number:d} is {digitalize:d} and is ", end="")
-if digitalize > 5:
-    print("greater than 5")
-elif digitalize == 0:
-    print("0")
+
+if number >= 0:
+    digitalize = number % 10
 else:
-    print("less than 6 and not 0")
+    digitalize = number % -10
+
+print("Last digit of {} is {}".format(number, digitalize), end='')
+
+if digitalize > 5:
+    print(" and is greater than 5")
+elif digitalize == 0:
+    print(" and is 0")
+else:
+    print(" and is less than 6 and not 0")
