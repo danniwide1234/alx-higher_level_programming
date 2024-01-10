71c92ce570041d10d46f1ad97396ab3943f81145
danniwide1234 <danniwide.1981@gmail.com>
2024-01-10 10:02:35 -0800

    1-search_replace.py

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+def search_replace(my_list, search, replace):
+   fresh_list = []
+    for element in my_list:
+        if element == search:
+            fresh_list.append(replace)
+        else:
+            fresh_list.append(element)
+    return fresh_list
