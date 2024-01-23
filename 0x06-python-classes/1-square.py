c9b6b7d68a08e22178cd9a86debd3de29a144830
danniwide1234 <danniwide.1981@gmail.com>
2024-01-23 09:39:42 -0800

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" class Square that defines a square"""
+
+
+class Square:
+    """ class Square that defines a square"""
+    def __init__(self, size):
+        """initializing square
+        Args:
+            size (int): size of the square
+        """
+        self.__size = size
