1fab07d3f22c394cae58cc027ac6405aa362980c
danniwide1234 <danniwide.1981@gmail.com>
2024-01-30 10:14:28 -0800

    python-test_driven dev

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,30 +1,17 @@
 #!/usr/bin/python3
-"""
-
-This module consist a function that adds two numbers
-
-"""
+"""Defining an integer addition function."""
 
 
 def add_integer(a, b=98):
-    """ Function adding two integer and/or float numbers
+    """Return the integer addition of a and b.
 
-    Args:
-        a:  number one
-        b:  number two
-
-    Returns:
-        addition of the two given numbers
+    Float arguments are typecasted to ints before addition is performed.
 
     Raises:
-        TypeError: If a or b aren't float numbers and/or integers
-
+        TypeError: If either of a or b is a non-integer and non-float.
     """
-
-    if not isinstance(a, int) and not isinstance(a, float):
+    if ((not isinstance(a, int) and not isinstance(a, float))):
         raise TypeError("a must be an integer")
-    if not isinstance(b, int) and not isinstance(b, float):
+    if ((not isinstance(b, int) and not isinstance(b, float))):
         raise TypeError("b must be an integer")
-    a = int(a)
-    b = int(b)
-    return (a + b)
+    return (int(a) + int(b))
--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,58 +1,35 @@
 #!/usr/bin/python3
-"""
-
-This module is consist of a function that divides the numbers of a matrix
-
-"""
+"""Defining a matrix division function."""
 
 
 def matrix_divided(matrix, div):
-    """ Function that divides the float/integer numbers of a matrix
+    """Dividing all elements of a matrix.
 
     Args:
-        matrix: list of a lists of floats/integers
-        div: number which divides the matrix
-
-    Returns:
-        A new matrix with the result of the division
-
+        matrix (list): A list of lists of floats or ints
+        div (int/float): The divisor.
     Raises:
-        TypeError: If the elements of the matrix aren't lists
-                   If the elemetns of the lists aren't floats/integers
-                   If div is not an float/integer number
-                   If the lists of the matrix don't have the same size
-
-        ZeroDivisionError: If div is zero
-
-
+        TypeError: If the matrix contains non-numbers.
+        TypeError: If the matrix contains rows of different sizes.
+        TypeError: If div is not an int or float.
+        ZeroDivisionError: If div is 0.
+    Returns:
+        A new matrix representing the result of the division.
     """
+    if (not isinstance(matrix, list) or matrix == [] or
+            not all(isinstance(row, list) for row in matrix) or
+            not all((isinstance(ele, int) or isinstance(ele, float))
+                    for ele in [number for row in matrix for number in row])):
+        raise TypeError("matrix must be a matrix (list of lists) of "
+                        "integers/floats")
 
-    if not type(div) in (int, float):
+    if not all(len(row) == len(matrix[0]) for row in matrix):
+        raise TypeError("Each row of the matrix must have the same size")
+
+    if not isinstance(div, int) and not isinstance(div, float):
         raise TypeError("div must be a number")
 
     if div == 0:
         raise ZeroDivisionError("division by zero")
 
-    msgs_type = "matrix must be a matrix (list of lists) of integers/floats"
-
-    if not matrix or not isinstance(matrix, list):
-        raise TypeError(msgs_type)
-
-    len_e = 0
-    msgs_size = "Each row of the matrix must have the same size"
-
-    for item in matrix:
-        if not item or not isinstance(item, list):
-            raise TypeError(msgs_type)
-
-        if len_e != 0 and len(item) != len_e:
-            raise TypeError(msgs_size)
-
-        for number in item:
-            if not type(number) in (int, float):
-                raise TypeError(msgs_type)
-
-        len_e = len(item)
-
-    f = list(map(lambda x: list(map(lambda y: round(y / div, 2), x)), matrix))
-    return (f)
+    return ([list(map(lambda x: round(x / div, 2), row)) for row in matrix])
