ccaac7f83ea025098dc39a96536023ee5c6abefc
danniwide1234 <danniwide.1981@gmail.com>
2024-02-05 10:05:00 -0800

    11-square.py

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,16 @@
+ a Rectangle subclass Square."""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Representing a square."""
+
+    def __init__(self, size):
+        """Initializing a new square.
+
+        Args:
+            size (int): the new square size.
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
