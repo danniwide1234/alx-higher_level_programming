90c4ac715c1519acca491c706c5a4cd1421fcb96
danniwide1234 <danniwide.1981@gmail.com>
2024-02-06 08:50:09 -0800

    11-student.py

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,37 +1,41 @@
 #!/usr/bin/python3
-""" class defining the class Student
-"""
+"""Defining a class Student."""
 
 
 class Student:
-    """ Class to create student instances """
+    """Representing a student."""
 
     def __init__(self, first_name, last_name, age):
-        """ Special method to initialize """
+        """Initializing a new Student.
+
+        Args:
+            first_name (str): student first name.
+            last_name (str): student last name.
+            age (int): student age.
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """ Method that returns directory description """
-        objects = self.__dict__.copy()
-        if type(x) is list:
+        """Get a dictionary representation of the Student.
 
-            for list_ item in x:
-                if type(list_item) is not str:
-                    return objects
+        If attrs is a list of strings, represents only those attributes
+        included in the list.
 
-            the_list = {}
-
-            for iatr in range(len(x)):
-                for p in objects:
-                    if attrs[iatr] == p:
-                        the_list[p] = objects[p]
-            return the_list
-
-        return objects
+        Args:
+            attrs (list): (Optional) The attributes to represent.
+        """
+        if (type(attrs) == list and
+                all(type(x) == str for x in attrs)):
+            return {m: getattr(self, m) for m in attrs if hasattr(self, m)}
+        return self.__dict__
 
     def reload_from_json(self, json):
-        """ Replaces all attributes of the Student instance """
-        for y in json:
-            self.__dict__[y] = json[y]
+        """Replace all attributes of the Student.
+
+        Args:
+            json (dict): The key/value pairs to replace attributes with.
+        """
+        for m, n in json.items():
+            setattr(self, m, n)
